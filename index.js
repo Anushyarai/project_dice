d1854b4522e8751857ba99319b2f23a89e8a42ad
Anushyarai <raianushya0@gmail.com>
2025-11-12 01:54:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-var randomNumber1=Math.floor(Math.random()*6)+1;
-var random1="images/dice" + randomNumber1 + ".png";
-var image1=document.querySelectorAll("img")[0];
-image1.setAttribute("src",random1);
-var randomNumber2=Math.floor(Math.random()*6)+1;
-var random2="images/dice" + randomNumber2 + ".png";
-var image2=document.querySelectorAll("img")[1];
-image2.setAttribute("src",random2);
-if(randomNumber1>randomNumber2){
-    document.querySelector("h1").innerHTML="Player 1 Wins";
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var random1 = "dice" + randomNumber1 + ".jpg";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", random1);
 
-}
-else if(randomNumber1<randomNumber2){
-    document.querySelector("h1").innerHTML="Player 2 Wins";
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var random2 = "dice" + randomNumber2 + ".jpg";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", random2);
 
-    
-}
-else{
-    document.querySelector("h1").innerHTML="Draws";
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins";
+} else {
+    document.querySelector("h1").innerHTML = "Draw";
 }
